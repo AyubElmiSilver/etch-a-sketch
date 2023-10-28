c22024045bb92b5dae0c8e8342378b3106b59722
AyubElmiSilver <09ayub10@gmail.com>
2023-10-28 15:00:53 +0100

    Create event listner for button with a for loop.
     This will run an event handler once the user clicks on the button.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+button.textContent = ('Select Size');
+button.style.backgroundColor = 'blue';
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', 'box')
-    container.appendChild(div);
-}
+button.addEventListener('click', () => {
+    let number = prompt('Enter number of squares per row and column for new grid');
+    if (number <= 100) {
+        for (let i = 0; i < (number*number); i++) {
+            const div = document.createElement('div');
+            div.setAttribute('id', 'box');
+            div.style.cssText = `width: ${(600 / number) - 2}px; height: ${(600 / number) - 2}px;`;
+            container.appendChild(div);
+        }
+    } 
 
-
-
+})
